ead6eb967e0c79b504cbd6c86d93e5a4cdc05f12
jfgalarce <jfgalarce@gmail.com>
2025-03-16 22:52:31 +0000

    se quita elemento sobrante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import Galeria from "@/components/home/Galeria";
 import Help from "@/components/home/Help";
 import Section1 from "@/components/home/Section1";
 import Services from "@/components/home/Services";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="w-full">
-      
       <Galeria />
       <About />
       <Help />
